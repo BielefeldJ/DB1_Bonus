0cb7f86d749bf289841cd78144a7dd3f842e928d
TheJens90 <287219a5@opayq.com>
2016-12-22 23:21:38 +0100

    project created

---
+++ src/de/fhdo/db/mysqldata/bielefeld/DataBielefeldDB.java
@@ -0,0 +1,12 @@
+
+package de.fhdo.db.mysqldata.bielefeld;
+
+//Logindata for Mysql DB from Bielefeld
+
+public class DataBielefeldDB
+{
+    public static final String IP = "";
+    public static final int PORT = 3306;
+    public static final String USER = "";
+    public static final String PASSWORD = "";     
+}
---
+++ src/de/fhdo/db/mysqldata/ruether/DataRuehterDB.java
@@ -0,0 +1,11 @@
+package de.fhdo.db.mysqldata.ruether;
+
+//Logindate for Mysql DB from Ruhter
+
+public class DataRuehterDB
+{
+    public static final String IP = "";
+    public static final int PORT = 3306;
+    public static final String USER = "";
+    public static final String PASSWORD = "";     
+}
