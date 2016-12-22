78d741d1d3640c6641ef56782b30fc76fa6b5eef
TheJens90 <287219a5@opayq.com>
2016-12-22 23:40:58 +0100

    Connection

--- src/de/fhdo/db/Main.java
+++ src/de/fhdo/db/Main.java
@@ -5,6 +5,12 @@
  */
 package de.fhdo.db;
 
+import de.fhdo.db.mysqldata.bielefeld.DataBielefeldDB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 /**
  *
  * @author Admin
@@ -13,6 +19,26 @@ public class Main
 {
     public static void main(String args[])
     {
+        String url="jdbc:mysql://"+ DataBielefeldDB.IP + ":"+DataBielefeldDB.PORT +"/buchhandlung";
+        String user=DataBielefeldDB.USER, passwd = DataBielefeldDB.PASSWORD;
+        
+        /* Login Leo
+        String url="jdbc:mysql://"+ DataRuehterDB.IP + ":"+DataRuehterDB.PORT +"/buchhandlung";
+        String user=DataRuehterDB.USER, passwd = DataRuehterDB.PASSWORD;
+        */
+                
+        
+        try(Connection con= DriverManager.getConnection(url,user,passwd);)
+        {
+            
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("Keine Verbindung zum Server");
+        }
+
+
+
         
     }
 }
